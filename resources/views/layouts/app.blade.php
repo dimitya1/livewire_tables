8deadef165a916cc6d6883ce3cc9f9633bac9ae7
DmitriyDerish <dimadee01@gmail.com>
2023-02-15 11:36:12 +0100

    Livewire installed, configured with pages basic layouts

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <!-- CSS modules -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <!-- JS modules -->
+    <script defer src="{{ asset('js/app.js') }}"></script>
+
+    <title>{{ isset($pageTitle) ? ($pageTitle . ' | ' . config('app.name')) : config('app.name') }}</title>
+
+    <!-- livewire styles -->
+    @livewireStyles
+
+    @yield('styles')
+</head>
+<body>
+
+@include('layouts.header')
+
+@yield('content')
+
+@include('layouts.footer')
+
+<!-- livewire scripts -->
+@livewireScripts
+
+@stack('scripts')
+
+</body>
+</html>
